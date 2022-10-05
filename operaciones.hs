6413dc0ab45000e2a03da69e8afc336ea8ea4cd2
juanmamanihumerez <91488752+juanmamanihumerez@users.noreply.github.com>
2022-10-04 23:13:33 -0400

    Add files via upload

---
+++ operaciones.hs
@@ -0,0 +1,12 @@
+suma:: Int -> Int -> Int
+suma x y = x+y
+
+resta:: Int -> Int -> Int
+resta x y = x-y
+
+multi:: Int -> Int -> Int
+multi x y = x*y
+
+divide:: Int -> Int -> Int
+divide x y = div x y
+
